4b4265562f395754821dd71f2b40024af84ce9ca
Eseoghene2012 <esadaheseangela@gmail.com>
2024-11-05 17:05:11 +0100

    Add files via upload

---
+++ SQLQuery1 CAPSTONE.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE CAPSTONE_PROJECT
+SELECT * FROM [dbo].[SalesData]
+
+SELECT * FROM [dbo].[CustomerData]
+
+SELECT Product, SUM(sales) AS Totalsales
+from [dbo].[SalesData]
+Group by Product
+
+
+select Region, count(orderID) AS Num_of_transaction
+from salesdata
+group by Region
+
+select Top(1) Product, SUM(Total_sales) AS Total_sales_value
+from salesdata 
+group by Product
+order by Total_sales Desc
+
+select product, sum(totalsales) as TotalRevenue
+from Salesdata
+group by product
+
+SELECT MONTH(ORDERDATE) AS MONTH,
+SUM(TOTAL_SALES) AS MONTHLYSALESTOTAL
+FROM SALESDATA WHERE YEAR(ORDERDATE) = 2024
+GROUP BY MONTH(ORDERDATE)
+ORDER BY MONTH
+
+SELECT TOP (5) CUSTOMER_ID,
+SUM(TOTAL_SALES) AS TOTALPURCHASEAMOUNT FROM SALESDATA GROUP BY CUSTOMER_ID
+ORDER BY TOTALPURCHASEAMOUNT DESC
+
+SELECT REGION, SUM(TOTAL_SALES) AS REGIONTOTALSALES,
+FORMAT(ROUND((SUM(TOTAL_SALES)/ CAST((SELECT SUM(TOTAL_SALES)
+FROM SALESDATA) AS DECIMAL(10,2))*100), 1),'0.#')
+
+SELECT PRODUCT FROM SALESDATA
+GROUP BY PRODUCT
+HAVING SUM(CASE WHEN ORDERDATE BETWEEN '2024-06-01' AND
+'2024-08-31'
+THEN 1 ELSE 0 END) = 0
